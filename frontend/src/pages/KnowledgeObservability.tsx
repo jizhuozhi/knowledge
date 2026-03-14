d435556416a1f645ab4ef5dec5f6c4c4d90709e1
jizhuozhi <jizhuozhi.george@gmail.com>
2026-03-14 13:55:32 +0800

    fix: add missing useCallback import in KnowledgeObservability.tsx

--- frontend/src/pages/KnowledgeObservability.tsx
+++ frontend/src/pages/KnowledgeObservability.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import {
   Alert,
